cb33053212f7e1ad9f8da215c687ed1e3b615bae
ThiagoSilva20 <thiago201714@gmail.com>
2022-02-15 13:04:24 -0300

    heroku

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x+9j7%#zt#8tr@u%dzikc)l!y&@^@7h6y*b*qbpieo-i*-#xk^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
