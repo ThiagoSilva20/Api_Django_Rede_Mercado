66139b225bc64a729ca0389260123ea61c1d7748
ThiagoSilva20 <thiago201714@gmail.com>
2022-03-31 15:57:28 -0300

    baixando inovaç[

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
